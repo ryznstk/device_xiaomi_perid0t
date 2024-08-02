6f237eccf5e89e12b39b353bbf644f0adb44fc92
Rafiester <rafi.haqqi@gmail.com>
2024-08-02 21:44:33 +0530

    peridot: Don't build qcom power hal
    Add TARGET_PROVIDES_POWERHAL := true
    to avoid conflict with caf powerhal
    
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -326,6 +326,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+TARGET_PROVIDES_POWERHAL := true
+
 # QMI
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor # Needed by CNE app
