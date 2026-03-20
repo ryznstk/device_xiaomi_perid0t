5793a44d047404c7c3a7ada88de06d61d2d6d38a
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-03-20 18:16:43 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ git clone -b lineage-23.2 https://gitlab.com/blu96/vendor-xiaomi-peridot-rb.git 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b lmks --depth 1 https://gitlab.com/blu96/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b lmk2 --depth 1 https://gitlab.com/blu96/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
 git clone -b auto --depth 1 https://gitlab.com/blu96/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
@@ -89,12 +89,12 @@ git reset --hard FETCH_HEAD
 croot
 
 # Refresh signing keys
-if [ -d vendor/lineage-priv/keys ]; then
+if [ -d vendor/evolution-priv/keys ]; then
   echo "Removing existing signing keys..."
-  rm -rf vendor/lineage-priv/keys
+  rm -rf vendor/evolution-priv/keys
 fi
 echo "Cloning fresh signing keys..."
-git clone https://github.com/droidcore/private_key.git -b main vendor/lineage-priv/keys
+git clone https://github.com/droidcore/private_key.git -b evo vendor/evolution-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then
