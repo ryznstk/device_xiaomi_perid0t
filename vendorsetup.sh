5cbddb840cf5ac9bb915bd88bdee356758ee55bf
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-03-20 15:22:04 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,13 +8,13 @@ git clone -b lineage-23.2 https://gitlab.com/blu96/vendor-xiaomi-peridot-rb.git 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone -b nonksu --depth 1 https://gitlab.com/blu96/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
+git clone -b lmks --depth 1 https://gitlab.com/blu96/kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
-git clone -b lineage-23.0 --depth 1 https://gitlab.com/blu96/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+git clone -b auto --depth 1 https://gitlab.com/blu96/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
-git clone -b lineage-23.2 --depth 1 https://github.com/peridot-dev/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
@@ -59,72 +59,22 @@ rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
 cd system/sepolicy
-git fetch https://github.com/ryznstk/lunaris_system_sepolicy.git test
-git reset --hard FETCH_HEAD
-croot
-
-cd build/soong
-git fetch https://github.com/ryznstk/build_soong test
-git reset --hard FETCH_HEAD
-croot
-
-cd external/arm-optimized-routines
-git fetch https://github.com/Lunaris-AOSP/external_arm-optimized-routines test
-git reset --hard FETCH_HEAD
-croot
-
-cd device/lineage/sepolicy
-git fetch https://github.com/Lunaris-AOSP/device_lineage_sepolicy 16.2
-git reset --hard FETCH_HEAD
-croot
-
-cd hardware/interfaces
-git fetch https://github.com/Lunaris-AOSP/hardware_interfaces.git test
-git reset --hard FETCH_HEAD
-croot
-
-cd packages/apps/Launcher3
-git fetch https://github.com/Lunaris-AOSP/packages_apps_Launcher3 test
-git reset --hard FETCH_HEAD
-croot
-
-cd packages/apps/FaceUnlock
-git fetch https://github.com/Lunaris-AOSP/packages_apps_FaceUnlock.git test
-git reset --hard FETCH_HEAD
-croot
-
-cd packages/modules/Bluetooth
-git fetch https://github.com/Lunaris-AOSP/packages_modules_Bluetooth 16.2
-git reset --hard FETCH_HEAD
-croot
-
-cd frameworks/base
-git fetch https://github.com/Lunaris-AOSP/frameworks_base test
-git reset --hard FETCH_HEAD
-croot
-
-cd frameworks/native
-git fetch https://github.com/Lunaris-AOSP/frameworks_native test
+git fetch https://github.com/ryznstk/system_sepolicy.git bq2
 git reset --hard FETCH_HEAD
 croot
 
 cd packages/apps/Settings
-git fetch https://github.com/ryznstk/packages_apps_Settings test
+git fetch https://github.com/ryznstk/evo_packages_apps_Settings bq2
 git reset --hard FETCH_HEAD
 croot
 
-cd packages/apps/Singularity 
-git fetch https://github.com/Lunaris-AOSP/packages_apps_Singularity test
+cd packages/apps/Evolver
+git fetch https://github.com/ryznstk/packages_apps_Evolver bq2
 git reset --hard FETCH_HEAD
 croot
 
-cd vendor/lineage
-git fetch https://github.com/Lunaris-AOSP/vendor_lineage 16.2
-git reset --hard FETCH_HEAD
-croot
-
-cd vendor/extras
-git fetch https://github.com/Lunaris-AOSP/vendor_extras test
+cd frameworks/base
+git fetch https://github.com/ryznstk/evo_frameworks_base bq2
 git reset --hard FETCH_HEAD
 croot
 
